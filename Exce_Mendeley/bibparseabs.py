e038e65fada6b5d1e63310ba8cefac28e2e538e0
fcavada <fcavada@gmail.com>
2019-11-23 19:20:38 +0000

    Removed abstracts with less than a threshold number of words nw_threshold = 80

--- Exce_Mendeley/bibparseabs.py
+++ Exce_Mendeley/bibparseabs.py
@@ -7,7 +7,7 @@ import argparse
 import sys
 import re
 
-MONTHS = 'January February March April May June July August September October November December'.split()
+
 
 def main():
     arg_parser = argparse.ArgumentParser(
@@ -33,42 +33,59 @@ def main():
         sys.exit(1)
 
 def print_entry(ent):
-    print('{ent.key} ({ent.typ}):'.format(ent=ent))
-    if 'title' in ent:
-        print('  ' + biblib.algo.tex_to_unicode(biblib.algo.title_case(
-            ent['title'], pos=ent.field_pos['title'])))
+    # print('{ent.key} ({ent.typ}):'.format(ent=ent))
+    # if 'title' in ent:
+    #     print('  ' + biblib.algo.tex_to_unicode(biblib.algo.title_case(
+    #         ent['title'], pos=ent.field_pos['title'])))
 
-    if 'author' in ent:
-        authors = [
-            biblib.algo.tex_to_unicode(author.pretty(),
-                                       pos=ent.field_pos['author'])
-            for author in ent.authors()]
-        if len(authors) == 0:
-            author = None
-        elif len(authors) == 1:
-            author = authors[0]
-        else:
-            author = ', '.join(authors[:-1])
-            if len(authors) > 2:
-                author += ','
-            if ent.authors()[-1].is_others():
-                author += ' et al.'
-            else:
-                author += ' and ' + authors[-1]
-        if author:
-            print('  By ' + author)
+    # if 'author' in ent:
+    #     authors = [
+    #         biblib.algo.tex_to_unicode(author.pretty(),
+    #                                    pos=ent.field_pos['author'])
+    #         for author in ent.authors()]
+    #     if len(authors) == 0:
+    #         author = None
+    #     elif len(authors) == 1:
+    #         author = authors[0]
+    #     else:
+    #         author = ', '.join(authors[:-1])
+    #         if len(authors) > 2:
+    #             author += ','
+    #         if ent.authors()[-1].is_others():
+    #             author += ' et al.'
+    #         else:
+    #             author += ' and ' + authors[-1]
+    #     if author:
+    #         print('  By ' + author)
 
-    if 'year' in ent:
-        if 'month' in ent:
-            mnum = ent.month_num()
-            print('  {} {}'.format(MONTHS[mnum - 1], ent['year']))
-        else:
-            print('  {}'.format(ent['year']))
-
+    # if 'year' in ent:
+    #     if 'month' in ent:
+    #         mnum = ent.month_num()
+    #         print('  {} {}'.format(MONTHS[mnum - 1], ent['year']))
+    #     else:
+    #         print('  {}'.format(ent['year']))
+    nw_treshold = 80
     if 'abstract' in ent:
-        print('  {}'.format(ent['abstract']))
-
-    print()
+        #print('  {}'.format(ent['abstract']))
+        #print('id---')
+        #print(ent.key)
+        #remove tabs
+        abstract= ent['abstract'].replace('\t','')
+        #remove new lines
+        abstract= abstract.replace('\n','')
+        #remove return of car
+        abstract= abstract.replace('\r','')
+        #print('abstract---')
+        #print(abstract)
+        #remove special characters in Latex format i.e. within curly brackets
+        abstract=re.sub('{.*}','',abstract)
+        # '.*' means any quantity and any type
+        words=abstract.split() 
+        num_words=len(words)
+        if num_words > nw_treshold:
+        #print(ent.key,'\t',len(abstract),'\t',abstract)
+        # comment previous line and activate below to only get key and num of characters to do a hystogram
+            print(ent.key,'\t',abstract)
 
 if __name__ == '__main__':
     main()
